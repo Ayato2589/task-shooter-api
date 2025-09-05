1f9e711d7139368fb8cf4ff4a73ac0406e8a8f5c
Ayato2589 <ayatoa2589@gmail.com>
2025-09-05 22:15:45 +0900

    CORS設定を追加

--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"https://task-shooter.vercel.app"},
+		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodOptions},
+	}))
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "hello task-shooter")
 	})
